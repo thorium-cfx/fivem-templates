0c648a792b376fc7d9c3cb02cb1f5d6ed3859be0
thorium <102315529+thorium-cfx@users.noreply.github.com>
2023-11-14 17:31:40 +0100

    Next try

--- .github/workflows/ci/build_windows.cmd
+++ .github/workflows/ci/build_windows.cmd
@@ -6,5 +6,13 @@ call fxd gen -game %PROGRAM%
 cd code\build\%PROGRAM%\
 IF %PROGRAM%==server ( cd windows )
 
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MsBuild.exe" CitizenMP.sln -t:build -restore -p:RestorePackagesConfig=true -p:preferredtoolarchitecture=x64 -p:configuration=release -maxcpucount:4 -v:q 2>&1 > errors.txt
-type errors.txt
+"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MsBuild.exe" CitizenMP.sln -t:build -restore -p:RestorePackagesConfig=true -p:preferredtoolarchitecture=x64 -p:configuration=release -maxcpucount:4 -v:q -flp1:logfile=errors.txt;errorsonly
+set MSBUILD_ERROR=%ERRORLEVEL%
+
+IF %MSBUILD_ERROR% EQU 0 (
+	echo Successfully build %PROGRAM%
+) else (
+	echo Failed to build %PROGRAM%, MSBuild returned with error code: %MSBUILD_ERROR%
+	for /F "tokens=*" %%A in (myfile.txt) do echo "::error::%%A"
+	exit /b %MSBUILD_ERROR%
+)
